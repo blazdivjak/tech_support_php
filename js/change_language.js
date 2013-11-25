76e7fd0d314a8f6e3d84049c96d50ee49f33965d
Blaz Divjak <blaz@divjak.si>
2013-11-25 19:34:09 +0100

    2.Phase of SP class at FRI. Javascript+JQuery. Missing translation .XML parsing.

--- js/change_language.js
+++ js/change_language.js
@@ -1,15 +1,44 @@
+/* Read a file  using xmlhttprequest  */
 
-
-importScripts('readxml.js');
+function loadXMLDoc(dname)
+{
+    if (window.XMLHttpRequest)
+    {
+        xhttp=new XMLHttpRequest();
+    }
+    else
+    {
+        xhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xhttp.open("GET",dname,false);
+    xhttp.send();
+    return xhttp.responseXML;
+}
 
 //change language with this function, and load translation
 function changeLanguage(){
 
+    var lang=event.currentTarget;
+
     //load XML
-    xmlDoc=loadXMLDoc("../static/translations/translations.xml");
+    xmlDoc=loadXMLDoc("static/translations/translations.xml");
+
+    //test
+    //alert(lang.id);
+
+    //Iterate over all translations for this page and change lang
+
+    //change element value
+    var element=document.getElementById("title").childNodes[0];
+    element.nodeValue="Test";
+
+    var element=document.getElementById("header").childNodes[0];
+    element.nodeValue="Test";
 
     //Set language cookie
 
-
 }
 
+
+document.getElementById('english').addEventListener('click', changeLanguage, false);
+document.getElementById('slovenian').addEventListener('click', changeLanguage, false);
