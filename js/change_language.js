0afcf4c6bb3cebe3810db2d05382bcf76683f8d3
Blaz Divjak <blaz@divjak.si>
2013-11-25 17:47:00 +0100

    2.Phase of SP class at FRI. Javascript+JQuery. Only missing translation activation and code optimization.

---
+++ js/change_language.js
@@ -0,0 +1,15 @@
+
+
+importScripts('readxml.js');
+
+//change language with this function, and load translation
+function changeLanguage(){
+
+    //load XML
+    xmlDoc=loadXMLDoc("../static/translations/translations.xml");
+
+    //Set language cookie
+
+
+}
+
